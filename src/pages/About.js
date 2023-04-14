a7a292b93a6343d432fefc3e15bf4c89a7dadfda
Amani Magloire <debabel1hackamani@gmail.com>
2023-04-14 21:52:28 +0000

    last commit

---
+++ src/pages/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ipsam
+        autem, a optio atque fuga in doloremque ea modi maxime dolorem. Ipsa
+        tenetur nesciunt, blanditiis autem voluptatum non numquam fugiat at sunt
+        sapiente placeat. Labore eligendi voluptatem officiis, minima minus
+        necessitatibus voluptate sed hic commodi exercitationem sint tenetur
+        suscipit enim? Sequi maxime reprehenderit mollitia officia quis neque
+        nulla amet, corrupti aliquid! Cumque quibusdam sunt, voluptatum quae
+        dolor veniam ab dolores nulla commodi maiores fugit consequuntur quaerat
+        officiis ad cupiditate consequatur ullam? Obcaecati totam, cupiditate
+        aliquam dicta possimus vitae nobis, reprehenderit earum aut vel aperiam
+        odio commodi repellendus labore assumenda quaerat facilis necessitatibus
+        blanditiis. Quis voluptates qui obcaecati dolor harum fugiat, non quod
+        nostrum unde soluta? Pariatur, tempora officiis. Sapiente, sed.
+      </p>
+    </div>
+  );
+};
+
+export default About;
